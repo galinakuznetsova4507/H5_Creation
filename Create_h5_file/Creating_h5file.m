1c392a046f96ce19a07eacbcae9718c059d253fd
brandalik <brandalik@eit.uni-kl.de>
2018-01-17 15:25:48 +0100

    temp version

--- Create_h5_file/Creating_h5file.m
+++ Create_h5_file/Creating_h5file.m
@@ -1,35 +1,42 @@
+%% Clear start
+clear; clc
 
-%% Creating H5File (converting)
-clear
-clc
-filename = 'DB23_5min_res_2016';
-data = load(filename);                                                      % loading mat-file
+%% Setup
+Input__filename = 'Example_FileFor_h5';
+Output_filename = 'Example_File';
 
-Files = fields(data.DB23_5min);                                             % sub-files from mat-file
-fid = H5F.create('DB23_5min.h5');                                           % creation of H5 file
+%% Check if file exists
+if isfile(Output_filename)
+    delete(Output_filename)     % TODO: Ask the user if he want to delete the file
+end
+
+%% Read in Input File
+data      = load(Input__filename);                                          % loading mat-file
+Database  = data.(cell2mat(fields(data)));                                  % call the loaded file 'Database'
+TabInDB   = fields(Database);                                               % Tables(fields) in Database DB
+fid = H5F.create(Output_filename);                                        	% creation of H5 file
 
 %% Creating the H5G group with names of subfiles 
-for i = 1:length(Files);
-    group{i} = H5G.create(fid,[Files{i}],size(Files{i},1));                 % group of subfiles
+for i = 1 : length(TabInDB)                                                 % over all Tables in DB
+    group = H5G.create(fid,[TabInDB{i}],size(TabInDB{i},1));                 % group of subfiles
     
-    tablevalues = data.DB23_5min.(Files{i});                                % values of all columns of the subfile
-    info = h5info('DB23_5min.h5');
+    tablevalues = data.(cell2mat(fields(data))).(TabInDB{i});                 % values of all columns of the subfile
+    info = h5info(Output_filename);
   
-%% Creating the H5D Dataset with values of the table
-    for j = 2:size((tablevalues),2);
-        heading{j}   = cell2mat(tablevalues.Properties.VariableNames(1,j)); % name of each column
-        parameter{j} = tablevalues.(j);                                     % values of one column
+    % Creating the H5D Dataset with values of the table
+    for j = 1:size((tablevalues),2)
+        heading   = cell2mat(tablevalues.Properties.VariableNames(1,j)); % name of each column
+        parameter = tablevalues.(j);                                     % values of one column
         
         type_id = H5T.copy('H5T_NATIVE_DOUBLE');                           
-        h5_dims = fliplr(size(parameter{j}));
+        h5_dims = fliplr(size(parameter));
         space_id = H5S.create_simple(2,h5_dims,h5_dims);                    % create a new data space
-        dset{j} = H5D.create(group{i},heading{j},type_id,space_id,...       % create new Dataset for all values
+        dset = H5D.create(group,heading,type_id,space_id,...       % create new Dataset for all values
             'H5P_DEFAULT');
         
-        fileattrib('DB23_5min.h5','+w');
-        H5D.write(dset{j},'H5ML_DEFAULT','H5S_ALL','H5S_ALL',...            % write a data to Dataset
-            'H5P_DEFAULT',parameter{j});
+        fileattrib(Output_filename,'+w');
+        H5D.write(dset,'H5ML_DEFAULT','H5S_ALL','H5S_ALL',...            % write a data to Dataset
+            'H5P_DEFAULT',parameter);
         
     end
 end
-
