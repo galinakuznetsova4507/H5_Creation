d056beb99bef667086937ce69aae2e2f0e6b894b
Владелец <@172.16.1.134>
2018-02-04 01:49:00 +0100

    Adding comments

--- Read_h5_file/Opening_h5file.m
+++ Read_h5_file/Opening_h5file.m
@@ -4,20 +4,19 @@ clear; close; clc
 
 %% Read in h5 file
 
-path_split  = strsplit(cd,'\');
-addpath      ([strjoin(path_split,'\'),'\Subfunctions\']);
+path_split  = strsplit(cd,'\');                                            % Split the path by "/"
+addpath      ([strjoin(path_split,'\'),'\Subfunctions\']);                 % Adding a folder Subfunctions in current folder
     
-h5_input.path = [strjoin(path_split(1:end),'\'),'\h5_files\'];
-h5_files_name = struct2table(dir(h5_input.path));
-h5_files_name = h5_files_name.name(h5_files_name.isdir ~= 1);   % Only files
+h5_input.path = [strjoin(path_split(1:end),'\'),'\h5_files\'];             % Path to the folder with H5 files
+h5_files_name = struct2table(dir(h5_input.path));                          % Content of folder h5_files (table)
+h5_files_name = h5_files_name.name(h5_files_name.isdir ~= 1);              % Only files
 
-All_Files = struct;                                  % Initial struct file
-for k = 1 : numel(h5_files_name)                     % Over all files
-    k_file_name   = strsplit(h5_files_name{k},'.');  % Without extension
-    h5_input.name = k_file_name{1};                  % File Name
-    h5table       = h5_2struct(h5_input);            % Read file as table
-    All_Files.(k_file_name{1}) = h5table;            % Save table as struct field
-    disp([num2str(k),'. File was read in.']);        % Info message
+All_Files = struct;                                                        % Initial struct file
+for k = 1 : numel(h5_files_name)                                           % Over all files
+    k_file_name   = strsplit(h5_files_name{k},'.');                        % Without extension
+    h5_input.name = k_file_name{1};                                        % File Name
+    h5table       = h5_2struct(h5_input);                                  % Read file as table
+    All_Files.(k_file_name{1}) = h5table;                                  % Save table as struct field
+    disp([num2str(k),'. File was read in.']);                              % Info message
 end
-
 
--- Read_h5_file/Subfunctions/h5_2struct.m
+++ Read_h5_file/Subfunctions/h5_2struct.m
@@ -1,5 +1,5 @@
 function h5table = h5_2struct(h5_input)
-%H5_2TABLE Creates an Table based on an h5 file
+%H5_2TABLE Creates a Table based on a h5 file
 %    Input:
 %       h5_info.Name - Name of the file
 %       h5_info.Path - Path of the file
@@ -8,16 +8,16 @@ function h5table = h5_2struct(h5_input)
 
 %% Main
 
-h5_path_name = [h5_input.path,h5_input.name,'.h5'];
-h5_content   = h5info(h5_path_name);
-Table_names  = {h5_content.Groups.Name};
+h5_path_name = [h5_input.path,h5_input.name,'.h5'];                        % Full path to the H5 file (with extension)
+h5_content   = h5info(h5_path_name);                                       % Content of H5 file
+Table_names  = {h5_content.Groups.Name};                                   % Names of tables from H5 file
 h5table      = struct;
 for k_T = 1 : numel(Table_names)
-    Table_names_clear           = erase(Table_names{k_T},'/');
-    h5table.(Table_names_clear) = table;
-    Column_names                = {h5_content.Groups(1).Datasets.Name};
-    for k_C = 1 : numel(Column_names)
-        h5table.(Table_names_clear).(Column_names{k_C}) = ...
-            h5read(h5_path_name,[Table_names{k_T},'/',Column_names{k_C}]);
-    end
+    Table_names_clear           = erase(Table_names{k_T},'/');             % deleting "/" from the name of table
+    h5table.(Table_names_clear) = table;                                   % empty table for converted H5 file
+    Column_names                = {h5_content.Groups(1).Datasets.Name};    % e.g. Variables
+    for k_C = 1 : numel(Column_names)                                      % over all columns
+        h5table.(Table_names_clear).(Column_names{k_C}) = ...              % Reading the column values and
+            h5read(h5_path_name,[Table_names{k_T},'/',Column_names{k_C}]); % assigning them to table 
+    end                                                                    
 end
